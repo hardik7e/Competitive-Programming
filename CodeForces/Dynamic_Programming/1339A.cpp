16323e351fdf26fa6bfca52806055a396c3fb383
H4D3N <62202998+H4D3N@users.noreply.github.com>
2020-11-11 11:13:29 +0530

    1339A Diamonds

---
+++ CodeForces/Dynamic_Programming/1339A.cpp
@@ -0,0 +1,35 @@
+      
+ /*     
+             _____        _    _             _   ___  ____
+            /  ___|  ____| \  | |___   ___   _  / _ \| __ \
+            \___  \ / __||  \ | | __| / __| | || | | |  __/
+             ___)  | |__ | | \| |\ \ | |__  | || |_| | |
+            |_____/ \___||_| \__|/_/  \___| |_| \___/|_|
+*/
+#define ll long long
+#define ub upper_bound
+#define lb lower_bound
+#define pb push_back
+#define plb pull_back
+#define mp make_pair
+#define ALL(v) v.begin(),v.end()
+#define SIZE(v) v.size()
+#define SORT(v) sort(ALL(v))
+
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    
+    int t;
+    cin>>t;
+    
+    while(t--){
+        int n;
+        cin>>n;
+        cout<<n<<'\n';
+    }
+    return 0;
+}
